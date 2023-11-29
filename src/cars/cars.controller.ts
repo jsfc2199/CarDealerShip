140815bbaed32584acc8af9d5fd7aa211753ca7d
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-11-29 18:15:49 -0500

    Obtener por ID
    Se crea n nuevo metodo para obtener un auto por ID de forma de entener como usar multiples gets en el controlador

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,10 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
-  
+
+    private cars = ['toyota', 'honda', 'Jeep'];
+
     @Get()
     getAllCars() {
-      return ['toyota', 'honda'];
+      return this.cars
+    }
+
+    //El get debe contener un segmento de url adicional para diferenciarse de otros
+    //Extraemos tambien el parametro que viene de la url, es decir obtener lo que viene en el :id y almacenarlo en id, usando @Param    
+    @Get(':id')
+    getCarById(@Param ('id') id:string){
+      return this.cars[id]      
     }
 }
