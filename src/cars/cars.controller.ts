c6e72222bc84435226e7d184a5caa008f7601af7
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-11-29 19:01:20 -0500

    CRUD
    Borrador de lo que sería un crud

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -20,5 +20,27 @@ export class CarsController {
     getCarById(@Param ('id', ParseIntPipe) id:number){
 
       return this.carsService.findById(id)      
+    }
+
+    @Post()
+    createCar( @Body() body: any ){
+      return{
+        body
+      }
+    }
+
+    @Patch(':id')
+    updateCar( @Param('id', ParseIntPipe )id: number){
+      return{
+        id
+      }
+    }
+
+    @Delete(':id')
+    deleteCar( @Param('id', ParseIntPipe) id: number ){
+      return{
+        method: 'delete',
+        id: id
+      }
     }
 }
